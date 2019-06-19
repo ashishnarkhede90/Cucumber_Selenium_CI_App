e38c007a01b45ea3890e2b9cc88f09e47977fb3f
Ashish Narkhede <ashish.narkhede90@gmail.com>
2019-06-19 12:18:47 +0530

    Fixing the script

--- build/setup_chrome_driver.sh
+++ build/setup_chrome_driver.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 # set up chrome driver
-chmod +x ../drivers/chromedriver_linux64 
+chmod +x drivers/chromedriver_linux64 
 ls -lrt drivers/
-sudo chmod 777 ../drivers/chromedriver_linux64
-ls -lrt ../drivers/
+sudo chmod 777 drivers/chromedriver_linux64
+ls -lrt drivers/
